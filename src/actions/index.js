ccec69d3c66f35f4587be27077377837f7cb14a8
Karnitskaya Kristina <kkarnitskaya@marathonbet.ru>
2016-07-21 17:27:01 +0300

    Add express server + add action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 export const FETCH_STANDS = 'FETCH_STANDS';
 
 export function fetchStands() {
-  const request = axios.get('data/data.json');
-  
+  const url = '/data';
+  const request = axios.get(url);
+
   return {
     type: FETCH_STANDS,
     payload: request
-  }
+  };
 }
--- src/reducers/reducer_stands.js
+++ src/reducers/reducer_stands.js
@@ -1,11 +1,13 @@
 import { FETCH_STANDS } from '../actions/index';
 
 export default function(state = [], action) {
+
   switch (action.type) {
     case FETCH_STANDS:
-      console.log(action.payload.data);
-      return [...state, action.payload.data];
-      break;
+      return [
+        ...state,
+        action.payload.data
+      ];
 
     default:
       return state;
